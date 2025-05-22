481483c2b66afca0c0330aab34e963b56eb3944d
Power <58568260+zeroootwo@users.noreply.github.com>
2025-05-22 21:56:23 +0300

    Обновление №3
    1. Были удалены ненужные скрипты со старого шаблона;
    2. Был изменен scripts.js - он делает две вещи: сбрасывает прокрутку страницы наверх при загрузке и Добавляет плавный скролл к секциям при клике на кнопки с классом next-btn;
    3. Созданы новые страницы: tutorials.html, my-work.html, best-choise.html, blender.html, maya.html, guides.html. Соотвественно и добавлен новый контент к некоторым из них.
    4. Создана новый каталог images с изображениями и видео для наполнения сайта

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-document.querySelectorAll('.dropdown-toggle').forEach(button => {
-  button.addEventListener('click', () => {
-    const menu = button.nextElementSibling;
-    menu.classList.toggle('active');
-  });
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
+
+document.querySelector('.next-btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
 });
